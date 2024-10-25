64ed5662701e49c10dea41b1962b0d78ada94acc
philipprobin <philipprobinsowik@gmail.com>
2024-10-25 11:48:16 +0700

    video with facecam

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -43,7 +43,7 @@ export function LandingPageComponent() {
               <div className="aspect-video mx-auto max-w-4xl">
                 <div style={{ padding: '51.94% 0 0 0', position: 'relative' }}>
                   <iframe
-                      src="https://player.vimeo.com/video/1022782946?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+                      src="https://player.vimeo.com/video/1023145185?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
                       frameBorder="0"
                       allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
                       style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
